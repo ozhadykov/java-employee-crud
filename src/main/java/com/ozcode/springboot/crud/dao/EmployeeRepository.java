238b35339135e994d913cb578275042878309036
Omar Zhadykov <omar3282088@gmail.com>
2025-10-12 16:28:37 +0200

    spring data rest example done

--- src/main/java/com/ozcode/springboot/crud/dao/EmployeeRepository.java
+++ src/main/java/com/ozcode/springboot/crud/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.ozcode.springboot.crud.dao;
 
 import com.ozcode.springboot.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
