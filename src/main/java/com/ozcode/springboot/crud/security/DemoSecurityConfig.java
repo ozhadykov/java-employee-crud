8c2e9745beed517829ea89a6eda56902166238c1
Omar Zhadykov <omar3282088@gmail.com>
2025-10-16 20:21:00 +0200

    added simple security and roles per request

--- src/main/java/com/ozcode/springboot/crud/security/DemoSecurityConfig.java
+++ src/main/java/com/ozcode/springboot/crud/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.ozcode.springboot.crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +35,29 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configuurer ->
+                configuurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use basic Auth
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request ForgeryCSRF
+        // in general not requiered for Stateless REST Apis that use POST, DELETE PUT ...
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
