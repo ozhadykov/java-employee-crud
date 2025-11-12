4fd11031554e77e04924513244f4a24d1b71e678
Omar Zhadykov <omar3282088@gmail.com>
2025-11-12 20:51:12 +0100

    started working on a simple mvc application

---
+++ src/main/java/com/ozcode/springboot/crud/controller/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.ozcode.springboot.crud.controller;
+
+import com.ozcode.springboot.crud.entity.Employee;
+import com.ozcode.springboot.crud.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for listing employees
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+
+        // get employees from the db
+        List<Employee> employees = this.employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", employees);
+
+        return "employees/list";
+    }
+}
